4b566c9f0dde9e15387c18414119fdb4572553f3
empty <zshgrlai@163.com>
2020-05-02 00:22:26 +0800

    .

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,6 +4,8 @@ const prod = require("./webpack.prod.conf");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = (env) => {
   console.log("环境", env);
@@ -14,26 +16,35 @@ module.exports = (env) => {
       filename: "bundle.js",
       path: path.resolve(__dirname, "../dist"),
     },
-    module:{
-        // 解析什么文件 test，用什么去转 loader
-        // loader 的3种写法 [],{} ,''
-        rules:[
+    module: {
+      // 解析什么文件 test，用什么去转 loader
+      // loader 的3种写法 [],{} ,''
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            isDev?"style-loader":MiniCssExtractPlugin.loader,
             {
-                test:/\.css$/,
-                use:['style-loader',{
-                    loader:'css-loader',
-                    options:{  //给loader 传递参数
-                        importLoaders:2
-                    }
-                },'postcss-loader','sass-loader']
+              loader: "css-loader",
+              options: {
+                //给loader 传递参数
+                importLoaders: 2,
+              },
             },
-            {
-                test:/\.scss$/,
-                use:['style-loader','css-loader','sass-loader']
-            },
-        ]
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
     },
     plugins: [
+      new MiniCssExtractPlugin({
+        filename: "css/main.css",
+      }),
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "../public/index.html"),
@@ -44,7 +55,7 @@ module.exports = (env) => {
           collapseWhitespace: true,
         },
       }),
-    ],
+    ].filter(Boolean),
   };
 
   if (isDev) {
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,3 +1,15 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 module.exports={
-    mode:'production'
-}
+    mode:'production',
+    optimization:{  //优化项目
+        minimizer:[
+            new OptimizeCssAssetsPlugin() //压缩css
+        ]
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+       
+    ]
+} 
