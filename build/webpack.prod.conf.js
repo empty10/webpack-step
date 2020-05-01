96af5d686d3abd7fd63faf16fe86041da73adbe4
empty <zshgrlai@163.com>
2020-05-02 00:24:57 +0800

    .

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,11 +1,13 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports={
     mode:'production',
     optimization:{  //优化项目
         minimizer:[
-            new OptimizeCssAssetsPlugin() //压缩css
+            new OptimizeCssAssetsPlugin(), //压缩css
+            new TerserWebpackPlugin()
         ]
     },
     plugins:[
